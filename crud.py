93c2ea75f1934658ea1b765750b5104618015a33
Mayaki <ujehmayaki@gmail.com>
2025-06-30 15:39:59 +0100

    created an insert statement which insert a dictionary into a database. it unpacks it first then insert into the DB

--- crud.py
+++ crud.py
@@ -15,4 +15,21 @@ cursor.execute('''
     ''')
 conn.commit()
 
+UserData = {
+    1:["Emmanuel", "Ujeh", "Mayaki", "17/10/1994", "Lagos"], 
+    2:["Glory", "Oiza", "Mayaki", "03/09/1997", "Lagos"],
+    3:["Osarumwense", "Peace", "Osayuwa", "10/02/1997", "Banin"]
+}
+
+UserData_list = [] #creat an empty list to store the unpacked dictionary key and values.
+
+for i,j in UserData.items(): #iterate over the dictionary 
+    unpack_UserData = (i, *j) #unpack the dictionry into a tuple with the asterick sign
+    print(unpack_UserData) 
+    UserData_list.append(unpack_UserData) #put the tupple inside the empty list
+
+    cursor.executemany("INSERT OR REPLACE INTO CRUD_TEST(ID, FIRSTNAME, MIDDLENAME, LASTNAME, DOB, CURRENT_CITY) VALUES(?,?,?,?,?,?)", UserData_list)
+    conn.commit()
+
+
 print("you have successfully created a new table")
